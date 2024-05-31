64277e461a4b37e0ef4d8acf13199819635dce0a
thamilthedal <57757731+thamilthedal@users.noreply.github.com>
2024-05-31 15:35:18 +0530

    Add files via upload

--- create_mesh.py
+++ create_mesh.py
@@ -3,28 +3,23 @@ from modules.create_blockmesh_dict import make_copy
 from modules.foam_to_fluent2D import main as fmp
 import modules.add_database as ad
 
-# R = "2.5e-3"
-# L = "0.75"
+R = "5e-3"
+L = "2.5"
 
-# mesh_settings = [
-#         [R, L, "180", "100"],
-#         [R, L, "375", "100"],
-#         [R, L, "750", "100"],
-#         [R, L, "375", "120"],
-#         [R, L, "375", "140"]
-#         ]
+mesh_settings = [
+        [R, L, "500", "80", "0.01", "30", "40"],
+        [R, L, "500", "100", "0.01", "30", "40"],
+        [R, L, "500", "120", "0.01", "30", "40"]
+        ]
 
-# mesh_settings = [[R, L, "100", "100"]]
+ID = "Z1"
 
-ID = "F1"
-fmp()
-
-# for n, i in enumerate(mesh_settings):
-#     mesh_name = f"M{n+1}-{ID}"
-#     make_copy(i)
-#     os.system("cp ./test ./mesh_ground/system/blockMeshDict")
-#     os.system("cd ./mesh_ground && blockMesh && foamMeshToFluent")
-#     fmp()
-#     os.system(f"mv ./mesh_ground/fluentInterface/mesh_ground_converted.msh ./mesh/{mesh_name}.msh")
-#     ad.add_database([mesh_name, ID, "2D", i[0], i[1], i[3], i[2]])
+for n, i in enumerate(mesh_settings):
+    mesh_name = f"M{n+1}-{ID}"
+    make_copy(i)
+    os.system("mv ./test ./mesh_ground/system/blockMeshDict")
+    os.system("cd ./mesh_ground && blockMesh && foamMeshToFluent")
+    fmp()
+    os.system(f"mv ./mesh_ground/fluentInterface/mesh_ground_converted.msh ./mesh/{mesh_name}.msh")
+    ad.add_database([mesh_name, ID, "2D", i[0], i[1], i[2], i[3], i[4], i[5], i[6]])
 
